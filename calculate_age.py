aec435de14a843a53c2b153cdb28d662d552a02c
bryanjob <74436343+bryanjob@users.noreply.github.com>
2024-05-10 17:06:36 -0500

    LEARN-4: resolve calculate age sample python file pylint issues

--- calculate_age.py
+++ calculate_age.py
@@ -2,7 +2,7 @@
 import datetime
 
 def get_age(yyyy:int, mm:int, dd:int) -> int:
-	""" This function return the age """
+    """ This function return the age """
     dob = datetime.date(yyyy, mm, dd)
     today = datetime.date.today()
     age = round((today - dob).days / 365.25)
