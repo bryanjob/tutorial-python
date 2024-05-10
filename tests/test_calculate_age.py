3574a645db77fe4addb5e81fd1e4ba6445e86a2b
bryanjob <74436343+bryanjob@users.noreply.github.com>
2024-05-10 17:25:30 -0500

    LEARN-4: resolve calculate age sample python file pylint issues

--- tests/test_calculate_age.py
+++ tests/test_calculate_age.py
@@ -1,9 +1,12 @@
+""" test file for calculate age """
 from calculate_age import get_age
 
 def test_get_age():
+    """ this test assert age is calculated corectly """
     # Given.
-    yyyy, mm, dd = map(int, "1996/07/11".split(""))   
+    yyyy, mm, dd = map(int, "1996/07/11".split(""))
     # When.
     age = get_age(yyyy, mm, dd)
     # Then.
     assert age == 26
+	
