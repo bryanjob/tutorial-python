aa4c3e4e165a6713bad0e58b38958030cfd46fe7
bryanjob <74436343+bryanjob@users.noreply.github.com>
2024-05-10 17:51:22 -0500

    LEARN-4: resolve test calculate age sample python file pytest issue

--- tests/test_calculate_age.py
+++ tests/test_calculate_age.py
@@ -4,7 +4,7 @@ from app.calculate_age import get_age
 def test_get_age():
     """ this test assert age is calculated corectly """
     # Given.
-    yyyy, mm, dd = map(int, "1996/07/11".split(""))
+    yyyy, mm, dd = map(int, "1996/07/11".split("/"))
     # When.
     age = get_age(yyyy, mm, dd)
     # Then.
