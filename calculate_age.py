7187b348f6b658280f53d7ba178661cf434208d4
bryanjob <74436343+bryanjob@users.noreply.github.com>
2024-05-10 17:01:25 -0500

    LEARN-4: reslove calculate age sample python file pylint issues

--- calculate_age.py
+++ calculate_age.py
@@ -1,8 +1,8 @@
 """ This module calculates the age """
 import datetime
 
-""" This function return the age """
 def get_age(yyyy:int, mm:int, dd:int) -> int:
+""" This function return the age """
     dob = datetime.date(yyyy, mm, dd)
     today = datetime.date.today()
     age = round((today - dob).days / 365.25)
