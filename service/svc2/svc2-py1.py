53e2d47d5ec772ea59f4e1bbebe4937e207e8c71
bryanjob <74436343+bryanjob@users.noreply.github.com>
2024-09-08 13:41:41 -0500

    LEARN-4: update lint workflow

--- service/svc2/svc2-py1.py
+++ service/svc2/svc2-py1.py
@@ -1,2 +1,7 @@
 # pylint: disable=invalid-name
 # pylint: enable=invalid-name
+# pylint: disable=trailing-whitespace
+# pylint: disable=unused-import
+# pylint: disable=missing-docstring
+import os
+import math
